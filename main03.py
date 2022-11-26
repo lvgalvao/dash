549f4d55d01fc74d8c12835ff4df640cecb5d06f
Luciano Filho <lvgalvaofilho@gmail.com>
2022-11-26 15:20:45 -0300

    add Input, Output

---
+++ main03.py
@@ -0,0 +1,30 @@
+from dash import Dash
+from dash_html_components import Div, P, Br
+from dash_core_components import Input as DCCInput
+from dash.dependencies import Input, Output
+
+
+app = Dash(__name__)
+
+app.layout = Div(
+    children=[
+        DCCInput(id="meu_input1", value="batatinha frita"),
+        Br(),
+        P(id="output1"),
+        Br(),
+        DCCInput(id="meu_input2", value="quantas saias a barata tem"),
+        Br(),
+        P(id="output2"),
+    ]
+)
+
+
+@app.callback(
+    [Output("output1", "children"), Output("output2", "children")],
+    [Input("meu_input1", "value"), Input("meu_input2", "value")],
+)
+def meu_callback(meu_input1, meu_input2):
+    return meu_input1, meu_input2
+
+
+app.run_server()
